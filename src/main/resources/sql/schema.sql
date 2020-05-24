6a7eb4d9335f6db07c6fc8e6970afd74e8221b93
dazhi <dazhi_6@163.com>
2020-05-24 23:21:51 +0800

    添加公司表，商品分类表，商品表

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -17,13 +17,39 @@ CREATE TABLE `role` (
  `name` varchar(30) NOT NULL COMMENT '角色名'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '用户表';
 
--- 商品表
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods` (
+-- 公司表
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE `company` (
  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
  `version` bigint(20) NOT NULL DEFAULT 0,
  `del_time` varchar(30),
- `cateid` BIGINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '商品分类id',
+ `name` varchar(30) NOT NULL COMMENT '公司名',
+ `type` varchar(30) NOT NULL COMMENT '公司类型',
+ `ceo` varchar(30) NOT NULL COMMENT '总裁',
+ `ceo_phone` varchar(30) NOT NULL COMMENT '总裁电话',
+ `createtime` TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '公司表';
+
+-- 无限级商品分类表
+DROP TABLE IF EXISTS `shop_category`;
+CREATE TABLE IF NOT EXISTS `shop_category` (
+ `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ `version` bigint(20) NOT NULL DEFAULT 0,
+ `del_time` varchar(30),
+ `company_id` BIGINT NOT NULL COMMENT '公司id',
+ `title` VARCHAR(32) NOT NULL DEFAULT '' COMMENT '标题',
+ `parentid` BIGINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '父级id',
+ `createtime` TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '无限级商品分类表';
+
+-- 商品表
+DROP TABLE IF EXISTS `shop_product`;
+CREATE TABLE `shop_product` (
+ `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ `version` bigint(20) NOT NULL DEFAULT 0,
+ `del_time` varchar(30),
+ `company_id` BIGINT NOT NULL COMMENT '公司id',
+ `category_id` BIGINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '商品分类id',
  `title` VARCHAR(200) NOT NULL DEFAULT '' COMMENT '商品标题',
  `description` TEXT COMMENT '商品描述',
  `num` BIGINT UNSIGNED NOT NULL DEFAULT '0' COMMENT '库存',
@@ -35,5 +61,5 @@ CREATE TABLE `goods` (
  `ishot` ENUM('0', '1') NOT NULL DEFAULT '0' COMMENT '是否热卖',
  `ison` ENUM('0', '1') NOT NULL DEFAULT '0' COMMENT '是否上架',
  `istui` ENUM('0', '1') NOT NULL DEFAULT '0' COMMENT '是否推荐',
- `createtime` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间'
+ `createtime` TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '创建时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '商品表';
